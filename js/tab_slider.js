71b6c3faaad3d75fd0adc83069607b4d957fab84
Public Keating <publickeating@me.com>
2011-05-04 22:38:29 -0600

    Placed content in an absolutely positioned panel that is shifted for smooth sliding.  The panel is wrapped by a clipping window with white gradient edges.

---
+++ js/tab_slider.js
@@ -0,0 +1,76 @@
+/*globals app */
+
+/** v-- Insert into script.js? --v **/
+app.tabSlider = (function() {
+  // Called on jQuery ready
+  app.ready.tabSlider = function() {
+    var a,
+        clip,
+        height,
+        links,
+        panel,
+        sections,
+        tabIndex,
+        tabs,
+        transitionFunc,
+        width;
+    
+    tabs = $('#tabs');
+    clip = tabs.find('#clip');
+    panel = clip.find('#panel');
+    sections = panel.find('section'); //$('#tabs #sections > section');
+    links = tabs.find('nav ul > li a');
+    
+    // The first tab is selected by default
+    tabIndex = 0;
+    
+    // Layout the individual sections left to right
+    width = 960;
+    $(panel).css('width', sections.length * width);
+    $.each(sections, function(index, section) {
+      $(section).css('left', index * width);
+    });
+    
+    // Show the panel once our sections are laid out
+    $(panel).show();
+    
+    // Resize to fit the first section
+    height = $(sections[0]).height();
+    $(clip).css('height', height);
+    
+    // The transition function
+    transitionFunc = function(e) {
+      var newTabIndex;
+      
+      // Determine the index of the new tab
+      newTabIndex = links.index(this);
+      
+      if (tabIndex !== newTabIndex) {
+        // 1. Remove active class from the current tab
+        $(links[tabIndex]).parent().toggleClass('active');
+        
+        // 2. Stretch/shrink the window to fit
+        height = $(sections[newTabIndex]).height();
+        $(clip).css('height', height);
+        
+        // 3. Make the new section visible
+        $(panel).animate({
+          left: -newTabIndex * width
+          }, 500, function() {
+          // Animation complete.
+        });
+        
+        // 4. Add active class to the new tab
+        $(links[newTabIndex]).parent().toggleClass('active');
+        
+        tabIndex = newTabIndex;
+      }
+    };
+    
+    // Add click events to the tab links
+    $.each(links, function(index, link) {
+      $(link).click(transitionFunc);
+    });
+  };
+})();
+/** ^-- Insert into script.js? --^ **/
