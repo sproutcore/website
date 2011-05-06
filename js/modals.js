6db2411feea7f4df7fc16456ff64fbd020b65dbe
Public Keating <publickeating@me.com>
2011-05-06 15:30:35 -0600

    Fade out a bit faster

--- js/modals.js
+++ js/modals.js
@@ -2,13 +2,11 @@
 
 /** v-- Insert into script.js? --v **/
 app.openModal = function(modal) {
-  $('#modal').fadeIn(250, function() {
-    // $(modal).show();
-  });
+  $('#modal').fadeIn(250);
   $(modal).fadeIn(500);
 };
 
 app.closeModal = function(modal) {
   $('#modal').fadeOut(250);
-  $(modal).fadeOut(750);
+  $(modal).fadeOut(500);
 };
