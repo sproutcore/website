684f0f5f567d0296dd1a4231c926c10e8c6533a8
Public Keating <publickeating@me.com>
2011-05-09 11:32:04 -0600

    Made modal background clickable to close the modal

--- content/js/modals.js
+++ content/js/modals.js
@@ -2,11 +2,11 @@
 
 /** v-- Insert into script.js? --v **/
 app.openModal = function(modal) {
-  $('#modal').fadeIn(250);
+  // $('#modal').fadeIn(250);
   $(modal).fadeIn(500);
 };
 
 app.closeModal = function(modal) {
-  $('#modal').fadeOut(250);
-  $(modal).fadeOut(500);
+  // $('#modal').fadeOut(250);
+  $(modal).fadeOut(250);
 };
