a349d2f5a61696ebbd33bd8e59f5a8a09a8e0e6c
Colin Campbell <colin.campbell@gmail.com>
2011-07-25 16:44:04 -0700

    Start bringing over style.css to use SCSS

---
+++ compass.rb
@@ -0,0 +1,9 @@
+project_path = File.dirname(__FILE__)
+http_path    = '/'
+output_style = :compressed
+sass_dir     = 'content/css'
+css_dir      = 'output/css'
+
+sass_options = {
+  :syntax => :scss
+}
